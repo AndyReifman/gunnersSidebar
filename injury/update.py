4353e3def50af366e84559cb38f4315564b7c93c
Andrew <andrew@reifman.net>
2019-12-24 21:10:53 +0000

    Completely rewrote the injury script to pull from Transfermarkt now that the arsenalreport api is dead.

--- injury/update.py
+++ injury/update.py
@@ -1,96 +1,109 @@
-#!/usr/bin/python2.7
-# coding: utf-8
-from unidecode import unidecode
-from time import sleep
-import requests
-import datetime
-import time
-import re
-import praw
-import pyotp
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-item = dict()
-f = open('/root/reddit/sidebar/injury/api.txt')
-apiKey = f.readline().splitlines()[0]
-#Get the injury list from arsenalreport.com
-def getInjuries():
-	resp = requests.get('https://www.arsenalreport.com/v1/injuries?api_key='+apiKey)
-	try:
-		for item in resp.json():
-			#Print out ongoing injuries
-	 		if item['end_date'] == None:
-				injury = item['injury_type']
-				injury = re.sub('\(([^\)]+)\)', '',injury)
-				time = item['expected_return']
-				if time == 0:
-					time = "Unknown"
-			 		#print item['player']['first_name'] + " " + item['player']['last_name'] + ": " + injury + " Unknown"
-				#else:
-			 		#print item['player']['first_name'] + " " + item['player']['last_name'] + ": " + injury + " " + str(time) + " weeks"
-	except:
-		print "Bad Request"
+import praw,re,logging,datetime,requests
+from unidecode import unidecode
+from bs4 import BeautifulSoup
 
 def getTimestamp():
-	dt = str(datetime.datetime.now().month) + '/' + str(datetime.datetime.now().day) + ' '
-        hr = str(datetime.datetime.now().hour) if len(str(datetime.datetime.now().hour)) > 1 else '0' + str(datetime.datetime.now().hour)
-        min = str(datetime.datetime.now().minute) if len(str(datetime.datetime.now().minute)) > 1 else '0' + str(datetime.datetime.now().minute)
-        t = '[' + hr + ':' + min + '] '
-        return dt + t
+    dt = str(datetime.datetime.now().month) + '/' + str(datetime.datetime.now().day) + ' '
+    hr = str(datetime.datetime.now().hour) if len(str(datetime.datetime.now().hour)) > 1 else '0' + str(datetime.datetime.now().hour)
+    min = str(datetime.datetime.now().minute) if len(str(datetime.datetime.now().minute)) > 1 else '0' + str(datetime.datetime.now().minute)
+    t = '[' + hr + ':' + min + '] '
+    return dt + t
 
 def loginBot():
     try:
         f = open('/root/reddit/sidebar/login.txt')
-        fkey = open('/root/reddit/sidebar/2fakey.txt')
-        admin,username,password,subreddit,user_agent,id,secret,redirect = f.readline().split('||',8)
-        key = fkey.readline().rstrip()
-        password += ':'+pyotp.TOTP(key).now()
+        #fkey = open('/root/reddit/sidebar/2fakey.txt')
+        admin,username,password,subreddit,user_agent,id,secret,redirect,refresh = f.readline().split('||',8)
+        #key = fkey.readline().rstrip()
+        #totp = pyotp.TOTP(key)
+        #password += ':'+totp.now()
         f.close()
-        fkey.close()
+        #fkey.close()
         r = praw.Reddit(client_id=id,
              client_secret=secret,
-             password=password,
-             user_agent=user_agent,
-             username=username)
-        print getTimestamp() + "OAuth session opened as /u/" + r.user.me().name
+             refresh_token=refresh.strip(),
+             user_agent=user_agent)
+        print(getTimestamp() + "OAuth session opened as /u/" + r.user.me().name)
         return r,admin,username,password,subreddit,user_agent,id,secret,redirect
-    except Exception, e:
-        print getTimestamp() + "Setup error \n"
-        sleep(5)
+    except (Exception, e):
+        print(getTimestamp() + str(e))
+        if str(e) == 'invalid_grant error processing request':
+            print(getTimestamp() + 'Attempting to log in again.\n')
+            time.sleep(5)
+            loginBot()
+            return
+        print(getTimestamp() + "Setup error \n")
 
-def buildSidebar():
-	injUrl = "http://www.arsenal.com/first-team/players/"
-	resp = requests.get('https://www.arsenalreport.com/v1/injuries?api_key='+apiKey)
-	body = "[//]: # (Injury Table)\n"
-	body += "| Player | Injury | Estimated Return |\n"
-	body += "|:------:|:-------:|:---------:|\n"
-	for item in resp.json():
-		#Print out ongoing injuries
- 		if item['end_date'] == None:
-			injury = item['injury_type']
-			injury = re.sub('\(([^\)]+)\)', '',injury)
-			time = item['expected_return']
-                        if unidecode(item['player']['first_name'].lower()) == "gabriel":
-                           playerUrl = "http://www.arsenal.com/first-team/players/gabriel"
-                        else:
-                            playerUrl = "http://www.arsenal.com/first-team/players/" + unidecode(item['player']['first_name'].lower()) + "-"+ unidecode(item['player']['last_name'].lower())
-			body += "|[" + item['player']['first_name'] + " " + item['player']['last_name'] + "](" + playerUrl + ")|" + injury + "|" + str(time) + "|\n"
-	body += "[//]: # (End Injury Table)"
-	return body
+def getNum(name):
+    return {
+        "Bernd Leno" : "1",
+        "Hector Bellerin" : "2",
+        "Kieran Tierney" : "3",
+        "Mohamed Elneny" : "4",
+        "Sokratis" : "5",
+        "Henrikh Mkhitaryan" : "7",
+        "Dani Ceballos" : "8",
+        "Alexandre Lacazette" : "9",
+        "Mesut Ozil" : "10",
+        "Lucas Torreira" : "11",
+        "Pierre-Emerick Aubameyang" : "14",
+        "Ainsley Maitland-Niles" : "15",
+        "Rob Holding" : "16",
+        "Nicolas Pepe" : "19",
+        "Shkodran Mustafi" : "20",
+        "Calum Chambers" : "21",
+        "David Luiz" : "23",
+        "Reiss Nelson" : "24",
+        "Emiliano Martinez" : "26",
+        "Konstantinos Mavropanos" : "27",
+        "Joe Willock" : "28",
+        "Matteo Guendouzi" : "29",
+        "Sead Kolasinac" : "31",
+        "Matt Macey" : "33",
+        "Granit Xhaka" : "34",
+        "Gabriel Martinelli" : "35",
+        "Bukayo Saka" : "77"
+    }[name]
+
+def getInjuries():
+    body = ''
+    address = 'https://www.transfermarkt.com/arsenal-fc/sperrenundverletzungen/verein/11'
+    #address = 'https://www.transfermarkt.com/manchester-city/sperrenundverletzungen/verein/281'
+    website = requests.get(address, headers={'User-Agent': 'Custom'})
+    html = website.text
+    soup = BeautifulSoup(html,"lxml")
+    table = soup.findAll('tbody')[0]
+    for x in range(1,len(table.findAll('tr')),3):
+        row = table.findAll('tr')[x]
+        if 'extrarow bg_blau_20 hauptlink' in str(row):
+            return body
+        name = unidecode(row.find('a',{'class','spielprofil_tooltip'}).getText())
+        injury = row.find('td',{'class','links hauptlink img-vat'}).getText()
+        date = row.findAll('td',{'class','zentriert'})[2].getText()
+        if date == '?':
+            date = 'Unknown'
+        num = getNum(name)
+        body += '|'+num+'|'+name + '|' + injury + '|' + date +'|\n'
+        return body
+        
+def buildTable():
+    body = "[//]: # (Injury Table)\n"
+    body += "|\\#| Player | Injury | Estimated Return |\n"
+    body += "|:------:|:------:|:-------:|:---------:|\n"
+    body += getInjuries()
+    body += "[//]: # (End Injury Table)"
+    return body
 
 
-#Update the sidebar
 def updateSidebar():
-	injTable = buildSidebar()		
-        r,admin,username,password,subreddit,user_agent,id,secret,redirect = loginBot()
-	settings = r.subreddit(subreddit).mod.settings()
-	contents = settings['description']
-	#We want to update current sidebar to where injury table goes
-	contents = re.sub('\[\/\/\]: # \(Injury Table\).*\[\/\/\]: # \(End Injury Table\)',injTable,contents,flags=re.DOTALL)
-	r.subreddit(subreddit).mod.update(description=contents)
+    table = buildTable()
+    r,admin,username,password,subreddit,user_agent,id,secret,redirect = loginBot()
+    settings = r.subreddit(subreddit).mod.settings()
+    contents = settings['description']
+    contents = re.sub('\[\/\/\]: # \(Injury Table\).*\[\/\/\]: # \(End Injury Table\)',table,contents,flags=re.DOTALL)
+    r.subreddit(subreddit).mod.update(description=contents)
+    print(getTimestamp() + "Injury Table Updated")
 
-
-
-
-getInjuries()
-updateSidebar()
-print getTimestamp() + "Injury Table Updated"
