947337ef3bfab39e1a0eaec4d5fe3c614728bbd0
Andrew Reifman-Packett <andrew@reifman.net>
2024-07-10 19:55:59 -0400

    Updating position filter

--- table/getTable.py
+++ table/getTable.py
@@ -9,48 +9,49 @@ from bs4 import BeautifulSoup
 
 def getSprite(teamName):
     return {
-         "AFC Bournemouth": "(#sprite1-p218)",
-         "Arsenal": "(#sprite1-p1)",
-         "Aston Villa": "(#sprite1-p19)",
-         "Bournemouth": "(#sprite1-p218)",
-         "Brentford":"(#sprite1-p198)",
-         "Brighton and Hove Albion" : "(#sprite1-p103)",
-         "Brighton & Hove Albion":"(#sprite1-p103)",
-         "Brighton":"(#sprite1-p103)",
-         "Burnley": "(#sprite1-p156)",
-         "Cardiff City": "(#sprite1-p80)",
-         "Chelsea": "(#sprite1-p4)",
-         "Crystal Palace": "(#sprite1-p67)",
-         "Everton": "(#sprite1-p15)",
-         "Fulham": "(#sprite1-p29)",
-         "Huddersfield Town":"(#sprite1-p199)",
-         "Hull City": "(#sprite1-p117)",
-         "Leeds United": "(#sprite1-p27)",
-         "Leicester City": "(#sprite1-p87)",
-         "Liverpool": "(#sprite1-p3)",
-         "Manchester City": "(#sprite1-p10)",
-         "Manchester United": "(#sprite1-p2)",
-         "Newcastle United": "(#sprite1-p11)",
-         "Norwich City": "(#sprite1-p44)",
-         "Middlesbrough": "(#sprite1-p91)",
-         "Sheffield United": "(#sprite1-p159)",
-         "Southampton": "(#sprite1-p38)",
-         "Stoke City": "(#sprite1-p81)",
-         "Sunderland": "(#sprite1-p46)",
-         "Swansea City": "(#sprite1-p39)",
-         "Tottenham Hotspur": "(#icon-poop)",
-         "Watford": "(#sprite1-p112)",
-         "West Bromwich Albion": "(#sprite1-p78)",
-         "West Ham United": "(#sprite1-p21)",
-         "Wolverhampton Wanderers": "(#sprite1-p70)",
-         "Wolves": "(#sprite1-p70)",
-        }[teamName]
+        "AFC Bournemouth": "(#sprite1-p218)",
+        "Arsenal": "(#sprite1-p1)",
+        "Aston Villa": "(#sprite1-p19)",
+        "Bournemouth": "(#sprite1-p218)",
+        "Brentford": "(#sprite1-p198)",
+        "Brighton and Hove Albion": "(#sprite1-p103)",
+        "Brighton & Hove Albion": "(#sprite1-p103)",
+        "Brighton": "(#sprite1-p103)",
+        "Burnley": "(#sprite1-p156)",
+        "Cardiff City": "(#sprite1-p80)",
+        "Chelsea": "(#sprite1-p4)",
+        "Crystal Palace": "(#sprite1-p67)",
+        "Everton": "(#sprite1-p15)",
+        "Fulham": "(#sprite1-p29)",
+        "Huddersfield Town": "(#sprite1-p199)",
+        "Hull City": "(#sprite1-p117)",
+        "Leeds United": "(#sprite1-p27)",
+        "Leicester City": "(#sprite1-p87)",
+        "Liverpool": "(#sprite1-p3)",
+        "Manchester City": "(#sprite1-p10)",
+        "Manchester United": "(#sprite1-p2)",
+        "Newcastle United": "(#sprite1-p11)",
+        "Norwich City": "(#sprite1-p44)",
+        "Middlesbrough": "(#sprite1-p91)",
+        "Sheffield United": "(#sprite1-p159)",
+        "Southampton": "(#sprite1-p38)",
+        "Stoke City": "(#sprite1-p81)",
+        "Sunderland": "(#sprite1-p46)",
+        "Swansea City": "(#sprite1-p39)",
+        "Tottenham Hotspur": "(#icon-poop)",
+        "Watford": "(#sprite1-p112)",
+        "West Bromwich Albion": "(#sprite1-p78)",
+        "West Ham United": "(#sprite1-p21)",
+        "Wolverhampton Wanderers": "(#sprite1-p70)",
+        "Wolves": "(#sprite1-p70)",
+    }[teamName]
 
 
 def getSign(goalDiff):
     if int(goalDiff) > 0:
-        return "+"+goalDiff
+        return "+" + goalDiff
     return goalDiff
+
 
 def teamsAbove(table, index, i):
     body = ""
@@ -58,50 +59,51 @@ def teamsAbove(table, index, i):
         return body
     elif index == 0:
         cells = table[0].findAll("td")
-        position = table[0].find("span",{"class","value"}).text
-        team = table[0].find("span",{"class","long"}).text
+        position = table[0].find("span", {"class", "league-table__value"}).text
+        team = table[0].find("span", {"class", "long"}).text
         goalDiff = cells[9].text.strip()
-        points = table[0].find("td",{"class","points"}).text
-        body += "|**"+position+"**|[]"+getSprite(team)+"|"+goalDiff+"|"+points+"|\n"
+        points = table[0].find("td", {"class", "points"}).text
+        body += "|**" + position + "**|[]" + getSprite(team) + "|" + goalDiff + "|" + points + "|\n"
     else:
-        for x in range(index, i,2):
+        for x in range(index, i, 2):
             cells = table[x].findAll("td")
-            position = table[x].find("span",{"class","value"}).text
-            team = table[x].find("span",{"class","long"}).text
+            position = table[x].find("span", {"class", "value"}).text
+            team = table[x].find("span", {"class", "long"}).text
             goalDiff = cells[9].text.strip()
-            points = table[x].find("td",{"class","points"}).text
-            body += "|**"+position+"**|[]"+getSprite(team)+"|"+goalDiff+"|"+points+"|\n"
+            points = table[x].find("td", {"class", "points"}).text
+            body += "|**" + position + "**|[]" + getSprite(team) + "|" + goalDiff + "|" + points + "|\n"
     return body
 
-def teamsBelow(table, index,i):
+
+def teamsBelow(table, index, i):
     body = ""
     if index < 10:
         index = 10
-    for x in range(i+2, index+2,2):
+    for x in range(i + 2, index + 2, 2):
         cells = table[x].findAll("td")
-        position = table[x].find("span",{"class","value"}).text
-        team = table[x].find("span",{"class","long"}).text
+        position = table[x].find("span", {"class", "league-table__value"}).text
+        team = table[x].find("span", {"class", "long"}).text
         goalDiff = cells[9].text.strip()
-        points = table[x].find("td",{"class","points"}).text
-        body += "|**"+position+"**|[]"+getSprite(team)+"|"+goalDiff+"|"+points+"|\n"
-        #Needed in case Arsenal are in 19th
+        points = table[x].find("td", {"class", "points"}).text
+        body += "|**" + position + "**|[]" + getSprite(team) + "|" + goalDiff + "|" + points + "|\n"
+        # Needed in case Arsenal are in 19th
         if x >= 38:
             return body
     return body
-    
+
 
 def findArsenal(table):
-    for index,row in enumerate(table):
+    for index, row in enumerate(table):
         if index % 2 == 1:
             continue
         cells = row.findAll("td")
-        position = row.find("span",{"class","value"}).text
-        team = row.find("span",{"class","long"}).text
+        position = row.find("span", {"class", "league-table__value"}).text
+        team = row.find("span", {"class", "long"}).text
         goalDiff = cells[9].text.strip()
-        points = row.find("td",{"class","points"}).text
+        points = row.find("td", {"class", "points"}).text
         if team == "Arsenal":
             i = index
-            body = "|**"+position+"**|[]"+getSprite(team)+"|**"+goalDiff+"**|**"+points+"**|\n"
+            body = "|**" + position + "**|[]" + getSprite(team) + "|**" + goalDiff + "**|**" + points + "**|\n"
             break
     topRange = i + 4
     botRange = i - 4
@@ -127,5 +129,6 @@ def main():
     body = findArsenal(table)
     return body
 
-if __name__== "__main__":
-    main()
+
+if __name__ == "__main__":
+    print(main())
