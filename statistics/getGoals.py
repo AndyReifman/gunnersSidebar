bef61ad8c6be8c74d4b0168d26bb6db4800f6dfc
Andrew Reifman-Packett <andrew@reifman.net>
2026-08-05 00:16:14 +0000

    Formatting

--- statistics/getGoals.py
+++ statistics/getGoals.py
@@ -7,6 +7,7 @@ from unidecode import unidecode
 
 players = []
 
+
 class Player(object):
     name = ""
     league = 0
@@ -15,7 +16,7 @@ class Player(object):
     eflcup = 0
     total = 0
 
-    def __init__(self,name,league,europa,facup,eflcup,total):
+    def __init__(self, name, league, europa, facup, eflcup, total):
         self.name = name
         self.league = league
         self.europa = europa
@@ -23,77 +24,106 @@ class Player(object):
         self.eflcup = eflcup
         self.total = total
 
+
 def printPlayers():
     for x in range(len(players)):
         p = players[x]
-        print(p.name +": " + str(p.league) +" " + str(p.champions) + " " + str(p.facup) + " " + str(p.eflcup) + " " + str(p.total))
+        print(
+            p.name
+            + ": "
+            + str(p.league)
+            + " "
+            + str(p.champions)
+            + " "
+            + str(p.facup)
+            + " "
+            + str(p.eflcup)
+            + " "
+            + str(p.total)
+        )
+
 
 def printPlayer(p):
-    print(p.name +": " + str(p.league) +" " + str(p.champions) + " " + str(p.facup) + " " + str(p.eflcup) + " " + str(p.total))
- 
+    print(
+        p.name
+        + ": "
+        + str(p.league)
+        + " "
+        + str(p.champions)
+        + " "
+        + str(p.facup)
+        + " "
+        + str(p.eflcup)
+        + " "
+        + str(p.total)
+    )
 
-def getStats(html,comp):
+
+def getStats(html, comp):
     soup = BeautifulSoup(html, "lxml")
-    table = soup.find("table",{"class":"items"})
+    table = soup.find("table", {"class": "items"})
     stats = table.find("tbody")
-    for row in stats.findAll("tr",{"class":['odd','even']}):
+    for row in stats.findAll("tr", {"class": ["odd", "even"]}):
         player = ""
-        #First up, get the name
-        #name = row.find("td",{"class":"posrela"}).findAll("a",{"class":"spielprofil_tooltip"})[0].getText()
-        name = row.find("td",{"class":"hauptlink"}).find_all('a', href=True)[0].getText()
-        columns = row.findAll("td",{"class":"zentriert"})
+        # First up, get the name
+        # name = row.find("td",{"class":"posrela"}).findAll("a",{"class":"spielprofil_tooltip"})[0].getText()
+        name = (
+            row.find("td", {"class": "hauptlink"}).find_all("a", href=True)[0].getText()
+        )
+        columns = row.findAll("td", {"class": "zentriert"})
         try:
             goals = columns[5].getText()
         except:
             goals = 0
-        if (goals == "-"):
+        if goals == "-":
             goals = 0
-        #goals = totalAssists[i]
-        #League 
+        # goals = totalAssists[i]
+        # League
         found = 0
         if comp == 0:
-            player = Player(name,goals,0,0,0,int(goals))
+            player = Player(name, goals, 0, 0, 0, int(goals))
             players.append(player)
-        #Champions League
+        # Champions League
         if comp == 1:
-            #Check to see if player already exists in the table
-            for index,p in enumerate(players):
+            # Check to see if player already exists in the table
+            for index, p in enumerate(players):
                 if p.name == name:
                     p.champions = goals
                     p.total += int(goals)
                     found = 1
                     break
-            #If the player doesn't exist we need to create them.
+            # If the player doesn't exist we need to create them.
             if not found:
-                player = Player(name,0,goals,0,0,int(goals))
+                player = Player(name, 0, goals, 0, 0, int(goals))
                 players.append(player)
-        #FA Cup
+        # FA Cup
         if comp == 2:
-            #Check to see if player already exists in the table
-            for index,p in enumerate(players):
+            # Check to see if player already exists in the table
+            for index, p in enumerate(players):
                 if p.name == name:
                     p.facup = goals
                     p.total += int(goals)
                     found = 1
                     break
-            #If the player doesn't exist we need to create them.
+            # If the player doesn't exist we need to create them.
             if not found:
-                player = Player(name,0,0,goals,0,int(goals))
+                player = Player(name, 0, 0, goals, 0, int(goals))
                 players.append(player)
-        #EFL Cup
+        # EFL Cup
         if comp == 3:
-            #Check to see if player already exists in the table
-            for index,p in enumerate(players):
+            # Check to see if player already exists in the table
+            for index, p in enumerate(players):
                 if p.name == name:
                     p.eflcup = goals
                     p.total += int(goals)
                     found = 1
                     break
-            #If the player doesn't exist we need to create them.
+            # If the player doesn't exist we need to create them.
             if not found:
-                player = Player(name,0,0,0,goals,int(goals))
+                player = Player(name, 0, 0, 0, goals, int(goals))
                 players.append(player)
-    return 
+    return
+
 
 def parseStats():
     body = ""
@@ -102,22 +132,26 @@ def parseStats():
     faCup = "https://www.transfermarkt.us/arsenal-fc/leistungsdaten/verein/11/plus/1?reldata=FAC%262025"
     champions_league = "https://www.transfermarkt.us/arsenal-fc/leistungsdaten/verein/11/plus/1?reldata=CL%262025"
     eflCup = "https://www.transfermarkt.us/arsenal-fc/leistungsdaten/verein/11/plus/1?reldata=CGB%262025"
-    premierLeagueWebsite = requests.get(premierLeague,headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
-    faCupWebsite = requests.get(faCup,headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
-    champions_league_website = requests.get(champions_league,headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
-    eflCupWebsite = requests.get(eflCup,headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+    # Previous user-agent Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36
+    premierLeagueWebsite = requests.get(premierLeague, headers={"User-Agent": "*"})
+    faCupWebsite = requests.get(faCup, headers={"User-Agent": "*"})
+    champions_league_website = requests.get(
+        champions_league, headers={"User-Agent": "*"}
+    )
+    eflCupWebsite = requests.get(eflCup, headers={"User-Agent": "*"})
     premier_html = premierLeagueWebsite.text
     fa_html = faCupWebsite.text
     champions_html = champions_league_website.text
     efl_html = eflCupWebsite.text
-    #Premier League
-    getStats(premier_html,0)
-    #Europa League
-    getStats(champions_html,1)
-    #FA Cup
-    getStats(fa_html,2)
-    #EFL Cup
-    getStats(efl_html,3)
+    # Premier League
+    getStats(premier_html, 0)
+    # Europa League
+    getStats(champions_html, 1)
+    # FA Cup
+    getStats(fa_html, 2)
+    # EFL Cup
+    getStats(efl_html, 3)
+
 
 def buildTable():
     players.sort(key=lambda x: x.total, reverse=True)
@@ -125,21 +159,34 @@ def buildTable():
     for x in range(0, 5):
         p = players[x]
         player = p.name
-        temp = player.replace(' ','-').lower()
+        temp = player.replace(" ", "-").lower()
         try:
             temp = unidecode(temp)
         except:
             pass
-        newLine = str(p.league) +"|" + str(p.champions) + "|" + str(p.facup) + "|" + str(p.eflcup) + "|" + str(p.total) + "|\n"
+        newLine = (
+            str(p.league)
+            + "|"
+            + str(p.champions)
+            + "|"
+            + str(p.facup)
+            + "|"
+            + str(p.eflcup)
+            + "|"
+            + str(p.total)
+            + "|\n"
+        )
         if newLine == "":
             continue
-        else:  
+        else:
             try:
-                if unidecode(temp) == 'alexis-sanchez':
+                if unidecode(temp) == "alexis-sanchez":
                     temp = "alexis"
             except:
                 pass
-            body += "|["+player+"](http://www.arsenal.com/men/players/"+temp+")|"
+            body += (
+                "|[" + player + "](http://www.arsenal.com/men/players/" + temp + ")|"
+            )
             body += newLine
     return body
 
@@ -149,6 +196,6 @@ def main():
     body = buildTable()
     return body
 
+
 if __name__ == "__main__":
     main()
-
